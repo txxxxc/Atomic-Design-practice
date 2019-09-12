150c5d4073705da2abaeb86e2cc6d64bb32a267a
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-09-12 23:14:55 +0900

    fix

--- src/components/atoms/balloon/index.js
+++ src/components/atoms/balloon/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './styles.css';
 
+console.log('hoge');
+
 const Balloon = ({ children, className, ...props }) => (
   <span className={[styles.balloon, className].join('')} {...props}>
     {children}
