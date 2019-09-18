152ab7c7fc51db1763bfdd2daddd859f69cd9bd4
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-09-18 23:58:16 +0900

    hoge

--- src/components/atoms/HolyGrailLayout/index.js
+++ src/components/atoms/HolyGrailLayout/index.js
@@ -4,3 +4,39 @@ import { containPresenter } from '../../utils/HoC.js';
 という記述がありますが、このタイミングではこの 1 行は必要ありません。
 記述するとエラーになりますので、この行は記述しないようにお願いします。
 */
+
+import React from 'react';
+import styles from './styles.css';
+
+const HolyGrailLayoutPresenter = ({
+  tag: Tag = 'div',
+  parts,
+  className,
+  ...props
+}) => {
+  const { top, bottom, main, left, right } = parts;
+  return (
+    <Tag className={[styles.root, className].join('')}>
+      {top}
+      <div className={styles.body}>
+        {main}
+        {left}
+        {right}
+      </div>
+      {bottom}
+    </Tag>
+  );
+};
+
+const HolyGrailLayoutContainer = ({ presenter, children, ...props }) => {
+  const parts = mapParts(children);
+  return presenter({ parts, ...props });
+};
+
+const partTypes = [
+  'HolyGrailTop',
+  'HolyGrailBottom',
+  'HolyGrailMain',
+  'HolyGrailLeft',
+  'HolyGrailRight'
+];
--- src/components/organisms/Notification/index.js
+++ src/components/organisms/Notification/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './styles.css';
+import MediaObjectLayout from '../../atoms/MediaObjectLayout/index.js';
 import Img from '../../atoms/Img/index.js';
 import Heading from '../../atoms/Heading/index.js';
 import { InfoTxt } from '../../atoms/Txt/index.js';
@@ -13,7 +14,11 @@ export class NotificationContainer extends Component {
   }
 
   render() {
-    const { presenter, onClickDelete:propsOnClickDelete, ...props } = this.props;
+    const {
+      presenter,
+      onClickDelete: propsOnClickDelete,
+      ...props
+    } = this.props;
     const onClickDelete = propsOnClickDelete ? this.onClickDelete : null;
     const presenterProps = { onClickDelete, ...props };
     return presenter(presenterProps);
@@ -29,28 +34,34 @@ export const NotificationPresenter = ({
   program,
   className,
   onClickDelete,
-  ...props,
+  ...props
 }) => (
-  <section className={ [ styles.root, className ].join(' ') } { ...props }>
-    <div>
-      <Img src={ program.thumbnail } className={ styles.media } width="128" height="72" />
-    </div>
-    <div className={ styles.body }>
-      <Heading level={ 3 } visualLevel={ 6 }>{ program.title }</Heading>
-      <InfoTxt size="s">{ program.channelName }</InfoTxt>
-      <InfoTxt size="s" className={ styles.time }>
-        <Time format="MM月DD日(ddd)HH:mm">{ program.startAt }</Time> ～
-        <Time format="HH:mm">{ program.endAt }</Time>
-      </InfoTxt>
-      <DeleteButton onClick={ onClickDelete } className={ styles.del } />
-    </div>
-  </section>
+  <MediaObjectLayout
+    tag="section"
+    className={[styles.root, className].join('')}
+    {...props}
+  >
+    <Img
+      src={program.thumbnail}
+      className={styles.media}
+      width="128"
+      height="72"
+    />
+    <Heading level={3} visualLevel={6}>
+      {program.title}
+    </Heading>
+    <InfoTxt size="s"> {program.channelName}</InfoTxt>
+    <InfoTxt size="s" className={styles.time}></InfoTxt>
+    <Time format="MM月DD日(ddd)HH:mm">{program.startAt}</Time> ~{' '}
+    <Time format="HH:mm">{program.endAt}</Time>
+    <DeleteButton onClick={onClickDelete} className={styles.del} />
+  </MediaObjectLayout>
 );
 
 const Notification = props => (
   <NotificationContainer
-    presenter={ presenterProps => <NotificationPresenter { ...presenterProps } /> }
-    { ...props }
+    presenter={presenterProps => <NotificationPresenter {...presenterProps} />}
+    {...props}
   />
 );
 
